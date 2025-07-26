33fca1bb16dedde25df0172cd06c139b73b63102
jen <121419174+jenboy@users.noreply.github.com>
2025-07-26 14:11:11 +0800

    chore: 更新导航数据 - 2025/7/26 14:11:10

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -65,34 +65,6 @@ export const mockData = {
       "order": 2,
       "sites": [
         {
-          "id": "vercel",
-          "name": "Vercel",
-          "url": "https://vercel.com",
-          "description": "前端部署平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/vercel.com.ico"
-        },
-        {
-          "id": "aliyun",
-          "name": "阿里云",
-          "url": "https://www.aliyun.com",
-          "description": "阿里巴巴云计算",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.aliyun.com.ico"
-        },
-        {
-          "id": "tencent-cloud",
-          "name": "腾讯云",
-          "url": "https://cloud.tencent.com",
-          "description": "腾讯云计算服务",
-          "icon": "https://external-content.duckduckgo.com/ip3/cloud.tencent.com.ico"
-        },
-        {
-          "id": "site-1752644060499",
-          "name": "怕死云",
-          "url": "https://www.pasyun.com/",
-          "description": "ipv6机器",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.pasyun.com.ico"
-        },
-        {
           "id": "cloudflare",
           "name": "Cloudflare",
           "url": "https://www.cloudflare.com",
@@ -105,6 +77,34 @@ export const mockData = {
           "url": "https://www.huaweicloud.com",
           "description": "华为云计算服务",
           "icon": "https://favicone.com/www.huaweicloud.com?s=64"
+        },
+        {
+          "id": "vercel",
+          "name": "Vercel",
+          "url": "https://vercel.com",
+          "description": "前端部署平台",
+          "icon": "https://favicone.com/vercel.com?s=64"
+        },
+        {
+          "id": "aliyun",
+          "name": "阿里云",
+          "url": "https://www.aliyun.com",
+          "description": "阿里巴巴云计算",
+          "icon": "https://favicone.com/www.aliyun.com?s=64"
+        },
+        {
+          "id": "tencent-cloud",
+          "name": "腾讯云",
+          "url": "https://cloud.tencent.com",
+          "description": "腾讯云计算服务",
+          "icon": "https://favicone.com/cloud.tencent.com?s=64"
+        },
+        {
+          "id": "site-1752644060499",
+          "name": "怕死云",
+          "url": "https://www.pasyun.com/",
+          "description": "ipv6机器",
+          "icon": "https://favicone.com/www.pasyun.com?s=64"
         }
       ]
     },
@@ -115,13 +115,6 @@ export const mockData = {
       "order": 6,
       "sites": [
         {
-          "id": "stackoverflow",
-          "name": "Stack Overflow",
-          "url": "https://stackoverflow.com",
-          "description": "程序员问答社区",
-          "icon": "https://external-content.duckduckgo.com/ip3/stackoverflow.com.ico"
-        },
-        {
           "id": "linuxdo",
           "name": "Linux.do",
           "url": "https://linux.do",
@@ -129,25 +122,32 @@ export const mockData = {
           "icon": "https://external-content.duckduckgo.com/ip3/linux.do.ico"
         },
         {
+          "id": "52pojie",
+          "name": "吾爱破解",
+          "url": "https://www.52pojie.cn/",
+          "description": "软件安全与破解技术论坛",
+          "icon": "https://external-content.duckduckgo.com/ip3/www.52pojie.cn.ico"
+        },
+        {
+          "id": "stackoverflow",
+          "name": "Stack Overflow",
+          "url": "https://stackoverflow.com",
+          "description": "程序员问答社区",
+          "icon": "https://favicone.com/stackoverflow.com?s=64"
+        },
+        {
           "id": "nodeseek",
           "name": "NodeSeek",
           "url": "https://www.nodeseek.com",
           "description": "极客技术社区",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.nodeseek.com.ico"
+          "icon": "https://favicone.com/www.nodeseek.com?s=64"
         },
         {
           "id": "v2ex",
           "name": "V2EX",
           "url": "https://www.v2ex.com",
           "description": "创意工作者社区",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.v2ex.com.ico"
-        },
-        {
-          "id": "52pojie",
-          "name": "吾爱破解",
-          "url": "https://www.52pojie.cn/",
-          "description": "软件安全与破解技术论坛",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.52pojie.cn.ico"
+          "icon": "https://favicone.com/www.v2ex.com?s=64"
         }
       ]
     },
@@ -158,27 +158,6 @@ export const mockData = {
       "order": 5,
       "sites": [
         {
-          "id": "binance",
-          "name": "币安",
-          "url": "https://www.binance.com",
-          "description": "加密货币交易平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.binance.com.ico"
-        },
-        {
-          "id": "okx",
-          "name": "OKX",
-          "url": "https://www.okx.com",
-          "description": "数字资产交易服务平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.okx.com.ico"
-        },
-        {
-          "id": "bitget",
-          "name": "Bitget",
-          "url": "https://www.bitget.com",
-          "description": "全球化数字资产交易服务商",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.bitget.com.ico"
-        },
-        {
           "id": "tonghuashun",
           "name": "同花顺",
           "url": "https://www.10jqka.com.cn",
@@ -186,32 +165,53 @@ export const mockData = {
           "icon": "https://external-content.duckduckgo.com/ip3/www.10jqka.com.cn.ico"
         },
         {
+          "id": "binance",
+          "name": "币安",
+          "url": "https://www.binance.com",
+          "description": "加密货币交易平台",
+          "icon": "https://favicone.com/www.binance.com?s=64"
+        },
+        {
+          "id": "okx",
+          "name": "OKX",
+          "url": "https://www.okx.com",
+          "description": "数字资产交易服务平台",
+          "icon": "https://favicone.com/www.okx.com?s=64"
+        },
+        {
+          "id": "bitget",
+          "name": "Bitget",
+          "url": "https://www.bitget.com",
+          "description": "全球化数字资产交易服务商",
+          "icon": "https://favicone.com/www.bitget.com?s=64"
+        },
+        {
           "id": "tradingview",
           "name": "TradingView",
           "url": "https://cn.tradingview.com",
           "description": "专业金融图表和交易平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/cn.tradingview.com.ico"
+          "icon": "https://favicone.com/cn.tradingview.com?s=64"
         },
         {
           "id": "xueqiu",
           "name": "雪球",
           "url": "https://xueqiu.com",
           "description": "聪明的投资者都在这里",
-          "icon": "https://external-content.duckduckgo.com/ip3/xueqiu.com.ico"
+          "icon": "https://favicone.com/xueqiu.com?s=64"
         },
         {
           "id": "coinbase",
           "name": "Coinbase",
           "url": "https://www.coinbase.com",
           "description": "美国合规加密货币交易平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.coinbase.com.ico"
+          "icon": "https://favicone.com/www.coinbase.com?s=64"
         },
         {
           "id": "futu",
           "name": "富途牛牛",
           "url": "https://www.futunn.com",
           "description": "港美股交易软件",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.futunn.com.ico"
+          "icon": "https://favicone.com/www.futunn.com?s=64"
         }
       ]
     },
@@ -222,20 +222,6 @@ export const mockData = {
       "order": 6,
       "sites": [
         {
-          "id": "mdn",
-          "name": "MDN Web Docs",
-          "url": "https://developer.mozilla.org",
-          "description": "Web开发权威文档",
-          "icon": "https://external-content.duckduckgo.com/ip3/developer.mozilla.org.ico"
-        },
-        {
-          "id": "w3school",
-          "name": "W3Schools",
-          "url": "https://www.w3schools.com",
-          "description": "Web技术教程",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.w3schools.com.ico"
-        },
-        {
           "id": "runoob",
           "name": "菜鸟教程",
           "url": "https://www.runoob.com",
@@ -243,18 +229,32 @@ export const mockData = {
           "icon": "https://external-content.duckduckgo.com/ip3/www.runoob.com.ico"
         },
         {
+          "id": "mdn",
+          "name": "MDN Web Docs",
+          "url": "https://developer.mozilla.org",
+          "description": "Web开发权威文档",
+          "icon": "https://favicone.com/developer.mozilla.org?s=64"
+        },
+        {
+          "id": "w3school",
+          "name": "W3Schools",
+          "url": "https://www.w3schools.com",
+          "description": "Web技术教程",
+          "icon": "https://favicone.com/www.w3schools.com?s=64"
+        },
+        {
           "id": "coursera",
           "name": "Coursera",
           "url": "https://www.coursera.org",
           "description": "在线课程平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.coursera.org.ico"
+          "icon": "https://favicone.com/www.coursera.org?s=64"
         },
         {
           "id": "site-1753429498680",
           "name": "樱花资源",
           "url": "https://www.xtvtv.com/",
           "description": "免费的图片资源网站",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.xtvtv.com.ico"
+          "icon": "https://favicone.com/www.xtvtv.com?s=64"
         }
       ]
     },
@@ -269,28 +269,28 @@ export const mockData = {
           "name": "JSON Formatter",
           "url": "https://jsonformatter.org",
           "description": "JSON格式化工具",
-          "icon": "https://external-content.duckduckgo.com/ip3/jsonformatter.org.ico"
+          "icon": "https://favicone.com/jsonformatter.org?s=64"
         },
         {
           "id": "regex101",
           "name": "Regex101",
           "url": "https://regex101.com",
           "description": "正则表达式测试",
-          "icon": "https://external-content.duckduckgo.com/ip3/regex101.com.ico"
+          "icon": "https://favicone.com/regex101.com?s=64"
         },
         {
           "id": "caniuse",
           "name": "Can I Use",
           "url": "https://caniuse.com",
           "description": "浏览器兼容性查询",
-          "icon": "https://external-content.duckduckgo.com/ip3/caniuse.com.ico"
+          "icon": "https://favicone.com/caniuse.com?s=64"
         },
         {
           "id": "tinypng",
           "name": "TinyPNG",
           "url": "https://tinypng.com",
           "description": "图片压缩工具",
-          "icon": "https://external-content.duckduckgo.com/ip3/tinypng.com.ico"
+          "icon": "https://favicone.com/tinypng.com?s=64"
         }
       ]
     },
@@ -305,28 +305,28 @@ export const mockData = {
           "name": "哔哩哔哩",
           "url": "https://www.bilibili.com",
           "description": "弹幕视频网站",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.bilibili.com.ico"
+          "icon": "https://favicone.com/www.bilibili.com?s=64"
         },
         {
           "id": "youtube",
           "name": "YouTube",
           "url": "https://www.youtube.com",
           "description": "视频分享平台",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.youtube.com.ico"
+          "icon": "https://favicone.com/www.youtube.com?s=64"
         },
         {
           "id": "zhihu",
           "name": "知乎",
           "url": "https://www.zhihu.com",
           "description": "知识问答社区",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.zhihu.com.ico"
+          "icon": "https://favicone.com/www.zhihu.com?s=64"
         },
         {
           "id": "douban",
           "name": "豆瓣",
           "url": "https://www.douban.com",
           "description": "文艺生活社区",
-          "icon": "https://external-content.duckduckgo.com/ip3/www.douban.com.ico"
+          "icon": "https://favicone.com/www.douban.com?s=64"
         }
       ]
     }
